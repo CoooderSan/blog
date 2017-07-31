3ab816801c80adb482ab93fe6529b9d6f0d146da
CoooderSan <546404101@qq.com>
2017-07-31 14:59:17 +0800

    将Django默认的数据库变更为mysql

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+	 'default':{
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'blog',
+		'USER': 'root',
+		'PASSWORD': 'Cc546404101!!',
+		'HOST': '127.0.0.1',
+		'PORT': '3306',
+	 }	
 }
 
 
